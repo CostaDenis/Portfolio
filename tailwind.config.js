b629ccb10ac3f2ece7569e82837aa2bc7225b0c4
DedeDoidao <deniscostaf@gmail.com>
2025-01-03 13:20:36 -0300

    Inicializando com os menus

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,17 @@ module.exports = {
   content: ["./src/**/*.{html,js}",
     './docs/**/*.{html,js}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'bgDark': '#1c1c1c',  //Background color for dark mode
+        'bgMenuTop': '#403c3c', //Background color for top menu
+        'bgMenuTopHover': '#484444', //Background color for top menu hover
+        'bgMenuLeft': '#383434', //Background color for left menu
+        'bgExplorer': '#282424', //Background color for explorer
+        'textColorMenu': '#bfbfbf', //Text color for dark mode
+
+      },
+    },
   },
   plugins: [],
 }
