fc32e6d3affc24bb865928086305070705619bd5
DedeDoidao <deniscostaf@gmail.com>
2025-01-03 15:53:56 -0300

    Adicionando balões no menu lateral

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,7 +8,8 @@ module.exports = {
         'bgDark': '#1c1c1c',  //Background color for dark mode
         'bgMenuTop': '#403c3c', //Background color for top menu
         'bgMenuTopHover': '#484444', //Background color for top menu hover
-        'bgMenuLeft': '#383434', //Background color for left menu
+        'bgMenuLeft': '#383434',   //Background color for left menu
+        'bgSpanMenu': '#252526', //Background color for span menu
         'bgExplorer': '#282424', //Background color for explorer
         'textColorMenu': '#bfbfbf', //Text color for dark mode
 
